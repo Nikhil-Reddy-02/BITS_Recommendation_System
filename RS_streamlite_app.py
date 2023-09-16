8ab6dc8e847886861f560d5abe36ea391484d9e3
Nikhil-Reddy-02 <116956174+Nikhil-Reddy-02@users.noreply.github.com>
2023-09-16 12:52:20 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -48,7 +48,7 @@ selected_movie = st.selectbox(
 
 if st.button('Show Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col1, col2, col3, col4, col5,col6,col7 = st.columns(8)
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
@@ -71,6 +71,6 @@ if st.button('Show Recommendation'):
     with col6:
         st.text(recommended_movie_names[6])
         st.image(recommended_movie_posters[6])
-    with col7:
+    with col8:
         st.text(recommended_movie_names[7])
         st.image(recommended_movie_posters[7])
